500e6bef3ce9fe1e626d465238c7e98223ecf476
Prakash1185 <pgcet601@gmail.com>
2024-12-31 21:41:48 +0530

    fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import HomePage from './pages/HomePage';
 import GamesPage from "./pages/GamesPage";
 import Navbar from "./components/Navbar";
@@ -16,7 +16,8 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/games" element={<GamesPage />} />
+        <Route path='/*' element={<Navigate to="/" />} />
+        {/* <Route path="/games" element={<GamesPage />} /> */}
       </Routes>
       {/* <PartyPopperButton /> */}
       {/* <Footer /> */}
